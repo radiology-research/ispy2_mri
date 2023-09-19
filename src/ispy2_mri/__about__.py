d36b955269669ca333dbd44638cd2a4c03bc1fd4
Ross Boylan <ross.boylan@ucsf.edu>
2023-09-19 15:38:46 -0700

    Stomp out more MIT License references. bump version (minor).

--- src/ispy2_mri/__about__.py
+++ src/ispy2_mri/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Ross Boylan <ross.boylan@ucsf.edu>
 #
-# SPDX-License-Identifier: MIT
-__version__ = "0.0.1"
+# SPDX-License-Identifier: GPL-3.0-only
+__version__ = "0.0.2"
--- src/ispy2_mri/__init__.py
+++ src/ispy2_mri/__init__.py
@@ -1,3 +1,3 @@
 # SPDX-FileCopyrightText: 2023-present Ross Boylan <ross.boylan@ucsf.edu>
 #
-# SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: GPL-3.0-only
